c16449d33200fa3baa46c915ccef3c880a88236b
Lonny Gomes <code@lonnygomes.com>
2014-02-09 01:17:40 -0500

    Made the python script more versital
    - it finds the proper lat/lon column indexes rather than relying on
      hardcoded column numbers
    - added a try/catch to continue in case one line of data is not valid

--- scripts/parseCrimeDataCoords.py
+++ scripts/parseCrimeDataCoords.py
@@ -15,6 +15,8 @@ except ImportError:
 isNAD83 = True
 coordsList = []
 outputFile = ""
+latCol = 8
+lonCol = 7
 
 if len(sys.argv) != 2:
     print 'Supply crimedata CSV!'
@@ -29,14 +31,19 @@ wgs84=pyproj.Proj("+init=EPSG:4326")   # WGS84 datum
 with open(csvFilename, 'r') as csvFile:
     reader = csv.reader(csvFile, delimiter=',')
 
+    curLine = 1
     wasSkipped = 0
     for row in reader:
         #we want to skip the first line
         if not wasSkipped:
             #check if it is LAT/LON data which seems
             #to be the format for data <= 2010
-            if row[8] == "LATITUDE": 
+            if "LATITUDE" in row: 
                 isNAD83 = False
+                #set the lat and lon columns
+                latCol = row.index("LATITUDE")
+                lonCol = row.index("LONGITUDE")
+                print("GOT COL NUM: %d" % latCol)
 
             wasSkipped = 1
             continue
@@ -44,17 +51,27 @@ with open(csvFilename, 'r') as csvFile:
         if isNAD83:
             #data is in NAD83 coordinates
             #lets grab them an convert to WGS84
-            curEastCoord = float(row[7])
-            curNorthCoord = float(row[8])
-            #print curNorthCoord, curEastCoord
-            curCoords = pyproj.transform(nad83, wgs84, curEastCoord, curNorthCoord)
+
+            try:
+                curEastCoord = float(row[lonCol])
+                curNorthCoord = float(row[latCol])
+                #print curNorthCoord, curEastCoord
+                curCoords = pyproj.transform(nad83, wgs84, curEastCoord, curNorthCoord)
+            except ValueError:
+                sys.stderr.write("\nCould not parse line number %d for %s. Continuing ...\n" % (curLine, csvFilename))
+                continue
         else:
             #data is already in Lat/Lon so we are golden
             #just make sure to pull from the correct columns
-            curCoords = [ float(row[9]), float(row[8]) ]
+            try:
+                curCoords = [ float(row[latCol]), float(row[lonCol]) ]
+            except ValueError:
+                sys.stderr.write("\nCould not parse line number %d for %s. Continuing ...\n" % (curLine, csvFilename))
+                continue
 
         #for now we are just dumping everything into arrays
         #coordsList.append({ "latitude" : curCoords[1], "longitude": curCoords[0]})
         coordsList.append([ round(curCoords[1], 6), round(curCoords[0], 6)])
+        curLine = curLine + 1
 
 print json.dumps(coordsList)
