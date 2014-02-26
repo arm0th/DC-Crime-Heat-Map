00e20c0439832ffe665c55b10dec8d85aedc49d7
Lonny Gomes <code@lonnygomes.com>
2014-02-25 19:54:49 -0500

    Added insertCommas method and corresponding unit tests

--- html/js/utils.js
+++ html/js/utils.js
@@ -19,11 +19,56 @@ function capitalizeStr(str) {
         });
 
     }
-    
+
     return retStr;
+}
+
+function insertCommas(num) {
+    "use strict";
+
+    var str = num.toString(),
+        chars = [],
+        curChar,
+        idx = 0,
+        digitIdx,
+        decimalDigits = "";
+
+    if (isNaN(str)) {
+        //if this isn't a number, lets not waste time
+        return "";
+    }
+
+    //check for negative number
+    if (Number(str) < 0) {
+        //remove the negative sign and add to stack
+        str = str.substr(1, str.length - 1);
+        chars.push("-");
+    }
+
+    //check for decimal
+    if (str.match(/[\-]*\d+\.[\d]+/)) {
+        //retreive the decimal portion of the number
+        decimalDigits = str.replace(/[\-]*\d+\./, ".");
+        //chop the decimal portion of the nummber off
+        str = str.replace(/\.\d+/, "");
+    }
+
+    idx = 0;
+    for (digitIdx = str.length; digitIdx >= 1; digitIdx--) {
+        //loop through each digit and determine where the commas go
+        if ((idx !== 0) && (digitIdx % 3 === 0)) {
+            chars.push(",");
+        }
+        curChar = str.substr(idx, 1);
+        chars.push(curChar);
+        idx++;
+    }
+
+    return chars.join("") + decimalDigits;
 }
 
 // export if running under node
 if (typeof exports !== 'undefined') {
     exports.capitalizeStr = capitalizeStr;
+    exports.insertCommas = insertCommas;
 }
--- test/utils-test.js
+++ test/utils-test.js
@@ -4,7 +4,7 @@ var utils = require("html/js/utils.js"),
 
 describe("utils", function () {
     "use strict";
-    
+
     describe("capitalizeStr", function () {
         it("should handle an empty string", function () {
             var result = utils.capitalizeStr("");
@@ -19,7 +19,6 @@ describe("utils", function () {
         it("should capitalize only the first letter of all uppercase letters", function () {
             var result = utils.capitalizeStr("THIS IS A TEST");
             result.should.equal("This is a test");
-            
         });
 
         it("should capitalize only the first letter of all lowercase letters", function () {
@@ -35,6 +34,69 @@ describe("utils", function () {
         it("should handle elipsese", function () {
             var result = utils.capitalizeStr("sentence one ... SENTENCE TWO.");
             result.should.equal("Sentence one ... sentence two.");
+        });
+    });
+
+    describe("insertCommas", function () {
+        it("should accept numbers", function () {
+            var result = utils.insertCommas(1);
+            result.should.equal("1");
+        });
+
+        it("should accept string representation of numbers", function () {
+            var result = utils.insertCommas("1");
+            result.should.equal("1");
+        });
+
+        it("should return an empty string for strings that aren't numbers", function () {
+            var result = utils.insertCommas("asdf");
+            result.should.equal("");
+        });
+
+        it("should accept three digit positive numbers", function () {
+            var result = utils.insertCommas("999");
+            result.should.equal("999");
+        });
+
+        it("should insert commas for positive whole numbers greater than 3 digits", function () {
+            var result = utils.insertCommas("3137");
+            result.should.equal("3,137");
+
+            result = utils.insertCommas("100000");
+            result.should.equal("100,000");
+
+            result = utils.insertCommas("123456789");
+            result.should.equal("123,456,789");
+        });
+
+        it("should accept 2 digit negative whole numbers", function () {
+            var result = utils.insertCommas("-99");
+            result.should.equal("-99");
+        });
+
+        it("should accept three digit negative whole numbers", function () {
+            var result = utils.insertCommas("-999");
+            result.should.equal("-999");
+        });
+
+        it("should accept positive decimal numbers", function () {
+            var result = utils.insertCommas("100.10");
+            result.should.equal("100.10");
+        });
+
+        it("should accept negative decimal numbers", function () {
+            var result = utils.insertCommas("-100.10");
+            result.should.equal("-100.10");
+        });
+
+        it("should accept positive decimal four digit numbers", function () {
+            var result = utils.insertCommas("1000.10");
+            result.should.equal("1,000.10");
+        });
+
+        it("should accept negative decimal four digit numbers", function () {
+            var result = utils.insertCommas("-1000.10");
+            result.should.equal("-1,000.10");
         });
     });
 });
