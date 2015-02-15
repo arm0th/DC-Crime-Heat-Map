2ad22fd243c5682b4243a73e46eac20021713216
Lonny Gomes <code@lonnygomes.com>
2015-02-15 13:18:02 -0500

    Moved over code from html -> client
    - Removed reference to Foundation, starting to use bootstrap

---
+++ client/app/workers/calcTotalsWorker.js
@@ -0,0 +1,38 @@
+/*jslint nomen: true */
+/*global importScripts, _, postMessage, self, onmessage,  insertCommas, capitalizeStr */
+importScripts("../../bower_components/underscore/underscore.js");
+importScripts("../utils.js");
+
+function calcTotals(data) {
+    "use strict";
+
+    var results = {},
+        curCrimeType,
+        finalArray = [];
+    _.each(data, function (val) {
+        curCrimeType = val[2];
+        if (!results[curCrimeType]) {
+            results[curCrimeType] = 1;
+        } else {
+            results[curCrimeType] += 1;
+        }
+    });
+
+    for (curCrimeType in results ) {
+        if (results.hasOwnProperty(curCrimeType)) {
+            finalArray.push({ offense: curCrimeType,
+                              offenseFormatted: capitalizeStr(curCrimeType),
+                              total: results[curCrimeType],
+                              totalFormatted: insertCommas(results[curCrimeType]),
+                              isVisible: true});
+        }
+    }
+
+    postMessage(finalArray);
+}
+
+onmessage = function (e) {
+    "use strict";
+
+    calcTotals(e.data);
+};
