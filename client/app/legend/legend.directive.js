9d7bd6658d586a5a1547972a3df11ca5b2afefc3
Lonny Gomes <code@lonnygomes.com>
2015-02-17 18:17:31 -0500

    Added basic toggle for legend
    It needs work, mainly handling the original case when data is first
    loaded into container

--- client/app/legend/legend.directive.js
+++ client/app/legend/legend.directive.js
@@ -11,7 +11,15 @@
                 },
                 templateUrl: 'app/legend/legend.html',
                 link: function (scope, element, attrs) {
-
+                    $("#legendToggleBtn").click(function () {
+                        var mapLegend = $("#mapLegend"),
+                            curPos = mapLegend.css("bottom"),
+                            contentHeight = $("#crimeTotals").outerHeight();
+                        console.log(curPos)
+                        console.log("YUP:" + $("#crimeTotals").outerHeight());
+                        TweenMax.to(mapLegend, 0.5, {
+                            bottom: (curPos === "0px") ? -contentHeight : "0px"});
+                    });
                 }
             };
         });
