3000397c21197e7ca4d3112132bc613f1e1b6ef2
Lonny Gomes <code@lonnygomes.com>
2015-04-27 00:04:20 -0400

    Copied functions from utils.js into new service

--- client/app/utils/utils.service.js
+++ client/app/utils/utils.service.js
@@ -1,4 +1,5 @@
 /*global angular */
+/*jslint nomen: true */
 (function () {
     'use strict';
 
@@ -7,13 +8,87 @@
             // Service logic
             // ...
 
-            var meaningOfLife = 42;
+            function capitalizeStr(str) {
+                var retStr = "",
+                    newStr,
+                    tokens;
+
+                if (str) {
+                    retStr = str;
+                    tokens = str.split(/[^\.]\.\s+/);
+
+                    tokens.forEach(function (s) {
+                        newStr = s.substr(0, 1).toUpperCase() +
+                                 s.substr(1, s.length - 1).toLowerCase();
+                        retStr = retStr.replace(s, newStr);
+                    });
+
+                }
+
+                return retStr;
+            }
+
+            function insertCommas(num) {
+                var str = num.toString(),
+                    chars = [],
+                    curChar,
+                    idx = 0,
+                    digitIdx,
+                    decimalDigits = "";
+
+                if (isNaN(str)) {
+                    //if this isn't a number, lets not waste time
+                    return "";
+                }
+
+                //check for negative number
+                if (Number(str) < 0) {
+                    //remove the negative sign and add to stack
+                    str = str.substr(1, str.length - 1);
+                    chars.push("-");
+                }
+
+                //check for decimal
+                if (str.match(/[\-]*\d+\.[\d]+/)) {
+                    //retreive the decimal portion of the number
+                    decimalDigits = str.replace(/[\-]*\d+\./, ".");
+                    //chop the decimal portion of the nummber off
+                    str = str.replace(/\.\d+/, "");
+                }
+
+                idx = 0;
+                for (digitIdx = str.length; digitIdx >= 1; digitIdx -= 1) {
+                    //loop through each digit and determine where the commas go
+                    if ((idx !== 0) && (digitIdx % 3 === 0)) {
+                        chars.push(",");
+                    }
+                    curChar = str.substr(idx, 1);
+                    chars.push(curChar);
+                    idx += 1;
+                }
+
+                return chars.join("") + decimalDigits;
+            }
+
+            function generateKey(str) {
+                var keyStr = "";
+
+                if (!str) {
+                    return "";
+                }
+
+                keyStr = str.toLowerCase()
+                            .replace(/[^a-zA-Z0-9]/g, "");
+
+
+                return keyStr;
+            }
 
             // Public API here
             return {
-                someMethod: function () {
-                    return meaningOfLife;
-                }
+                capitalizeStr: capitalizeStr,
+                insertCommas: insertCommas,
+                generateKey: generateKey
             };
         });
 }());
